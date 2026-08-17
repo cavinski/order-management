dba9e63d9fef055466dccf424f9ad79432223a24
Caio Pedro <caiopedrovianadasilva@email.com>
2026-08-17 17:34:54 -0300

    feat: add UserService

---
+++ src/main/java/com/caio/ordermanagement/user/UserService.java
@@ -0,0 +1,26 @@
+package com.caio.ordermanagement.user;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+    
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public User createUser(String name, String email, String password) {
+
+        if(userRepository.existsByEmail(email)) {
+            throw new IllegalArgumentException("Email already in use");
+        }
+
+        User user = new User(name, email, password);
+
+        return userRepository.save(user);
+    }
+}
